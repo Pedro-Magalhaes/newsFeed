0bd2df0308b379ed71a8b4fbd6564d4ef0aa98d0
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2019-06-06 08:46:39 -0300

    alterando arquitetura

--- meuScript.js
+++ meuScript.js
@@ -4,8 +4,8 @@ const baseUrl = "https://newsapi.org/v2/top-headlines"
 const apiKey = "a36cd6c4d2034e688b4ecc8b0e22b548";
 
 // default values
-const baseCountry = "br";
-const baseCategory = "business"
+let selectedCountry = "br";
+let selectedCategory = "general"
 const pageSize = 20;
 const basePage = 0;
 
@@ -16,16 +16,8 @@ xhttp.onreadystatechange = function() {
        // Typical action to be performed when the document is ready:
        let json = JSON.parse(xhttp.responseText);
        let text = json.totalResults + "  " + json.status;
-       let content = "";
-       for (let index = 0; index < json.articles.length; index++) {
-        content = content + "<div class='article'> " + `<a class="article_link" target="_blank" href=${json.articles[index].url}>`+ 
-                            "<h2 class=\"article_title\">" + json.articles[index].title + 
-                            "</h2> <p class=\"article_description\">" + json.articles[index].description +
-                            "</p> </a> </div> " ;
-           
-       }
-       document.getElementById("mainDiv").innerHTML = content;
-    }
+       buildView(json.articles);
+	}
 };
 
 function buildUrl(base, parametersArray) {
@@ -43,8 +35,8 @@ function buildUrl(base, parametersArray) {
 // query String representando a keyword para busca
 // page inteiro para paginação de 0 a (total result/ pageSize)
 function loadResults(country, category, query, page) {
-    country = country || baseCountry;
-    category = category || baseCategory;
+    country = country || selectedCountry;
+    category = category || selectedCategory;
     page = page || basePage;
 
     const UrlParametersArray =  [`country=${country}`, `category=${category}`,
@@ -54,11 +46,42 @@ function loadResults(country, category, query, page) {
         UrlParametersArray.push(`q=${query}`);
     } 
     const url = buildUrl( baseUrl, UrlParametersArray );
-    console.log(url);
     xhttp.open("GET", url , true);
     xhttp.send();
 }
 
+function submit() {
+	loadResults(selectedCountry,selectedCategory);
+}
+
+function change(selected) {
+	if( selected.id == "categoria" ) {
+		selectedCategory = selected.options[selected.selectedIndex].value;
+	}
+	else if( selected.id == "pais" ) {
+		selectedCountry = selected.options[selected.selectedIndex].value;
+	}
+	else {
+		console.log("ERRO");
+	}
+	
+	
+}
+
+
+function buildView(elements) {
+	 let content = "";
+	 for (let index = 0; index < elements.length; index++) {
+		content = content + "<div class='article'> " + `<a class="article_link" target="_blank" href=${elements[index].url}>`+ 
+							"<h2 class=\"article_title\">" + elements[index].title + 
+							"</h2> <p class=\"article_description\">" + elements[index].description +
+							"</p> </a> </div> " ;
+		   
+	   }
+	   document.getElementById("mainDiv").innerHTML = content;
+}
 
 // chamada inicial
-loadResults(baseCountry,baseCategory,"",basePage);
+loadResults(selectedCountry,selectedCategory,"",basePage);
+
+
