c9956d21694efa70c5418d07aa688aabd5a246e4
Leonardo Lages <leonardolages@hotmail.com>
2019-07-02 08:56:12 -0300

    Adicionando etvx
    relacionado à issue #2

--- DOMinput.js
+++ DOMinput.js
@@ -6,18 +6,22 @@ let selectedCategory = "general"
 const pageSize = 12;
 const basePage = 1;
 let currentPage = basePage;
-let searchType = 'categoria';
-let query = "";
 
+
+/*
+	Recebe uma string e altera as propriedades de alguns elementos com base nisso
+	PRÉ: type é uma string com valor 'categoria' ou 'busca'
+		(Verificação: as condicionais só resultam em alguma ação nesses dois casos)
+	PÓS: de acordo com o valor de type, altera a classe dos botões btn_categoria e btn_busca
+		e esconde ou exibe as categorias e a barra de busca
+*/
 function changeSearch( type ) { // TODO fazer logica de mudança
 	if( type == 'categoria' ) {
-		searchType = 'categoria';
 		document.getElementById('btn_categoria').classList.add("selecionado");
 		document.getElementById('btn_busca').classList.remove("selecionado");
 		document.getElementById('queryInput').style.display = 'none';
 		document.getElementById('categoria').style.display = 'flex';
 	} else if ( type == 'busca' ) {
-		searchType = 'busca';
 		document.getElementById('btn_busca').classList.add("selecionado");
 		document.getElementById('btn_categoria').classList.remove("selecionado");
 		document.getElementById('categoria').style.display = 'none';
@@ -27,12 +31,14 @@ function changeSearch( type ) { // TODO fazer logica de mudança
 
 function submit() {
 	currentPage = basePage;
+	console.log(pageSize);
 	loadResults(selectedCountry,selectedCategory, currentPage ,pageSize);
 }
 
 function submitSearch() {
-	query = getQueryInputText();
+	let query = getQueryInputText();
 	currentPage = basePage;
+	console.log(pageSize);
 	searchByQuery(query,currentPage,pageSize);
 }
 
@@ -59,13 +65,4 @@ function getQueryInputText() {
 	}
 	return '';
 	
-}
-
-function seeMore() {
-	currentPage = currentPage + 1;
-	if(searchType == 'categoria') {
-		loadResults(selectedCountry,selectedCategory, currentPage ,pageSize, true);
-	} else {
-		searchByQuery(query,currentPage,pageSize, true);
-	}	
 }
--- requests.js
+++ requests.js
@@ -3,17 +3,27 @@ const xhttp = new XMLHttpRequest();
 const baseUrl = "https://newsapi.org/v2/top-headlines";
 const apiKey = "a36cd6c4d2034e688b4ecc8b0e22b548";
 const everythingUrl = "https://newsapi.org/v2/everything";
-let global_append = false;
-// declarando o listener da chamada http
+
+/*
+	Listener que recebe a resposta da chamada à API
+	PRÉ: resposta da chamada à API em xhttp.responseText (um array de elementos jsons)
+	PÓS: chamada à função buildView, passando como parâmetro um array de jsons das notícias
+*/
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
        let json = JSON.parse(xhttp.responseText);
        let text = json.totalResults + "  " + json.status;
-       buildView(json.articles, global_append);
+       buildView(json.articles);
 	}
 };
 
+/*
+	Monta uma string com base nos parâmetros e url base fornecidos
+	PRÉ: base é uma string e parametersArray é um array de strings
+	PÓS: uma string concatenando a string 'base' com a chave da API (global) ou
+		o array de parâmetros separados pelo caractere '&'
+*/
 function buildUrl(base, parametersArray) {
     base = base || baseUrl;
     if( !parametersArray ) {
@@ -24,17 +34,19 @@ function buildUrl(base, parametersArray) {
 }
 
 
-
-// recebe o pais a categoria e a query para a chamada à Api
-// se qualquer um deles for nulo utilizamos o default
-// country sigla do pais, category string representando a categoria
-// query String representando a keyword para busca
-// page inteiro para paginação de 1 a (total result/ pageSize)
-function loadResults(country, category, page, pageSize, append = false) {
-	global_append = append;
+/*
+	Recebe o país, a categoria, a página e o tamanho de página e realiza a chamada à API
+	PRÉ: country é uma string com a sigla do país (dentre os permitidos pela API),
+		category é uma categoria dentre as permitidas pela API,
+		page é um inteiro maior que zero,
+		pageSize é um inteiro maior que zero
+	PÓS: é realizada a chamada à API conforme as variáveis entradas
+*/
+function loadResults(country, category, page, pageSize) {
     country = country || selectedCountry;
     category = category || selectedCategory;
     page = page || basePage;
+	console.log(pageSize);
     const UrlParametersArray =  [`country=${country}`, `category=${category}`,
                                  `page=${page}`, `apiKey=${apiKey}`,`pageSize=${pageSize}`]
     
@@ -43,8 +55,15 @@ function loadResults(country, category, page, pageSize, append = false) {
     xhttp.send();
 }
 
-function searchByQuery(query, page, pageSize, append = false) {
-	global_append = append;
+/*
+	Recebe uma busca, a página e o tamanho de página e realiza a chamada à API
+	PRÉ: query é uma string de busca para a chamada à API
+		page é um inteiro maior que zero,
+		pageSize é um inteiro maior que zero
+	PÓS: é realizada a chamada à API conforme as variáveis entradas
+*/
+function searchByQuery(query, page, pageSize) {
+	console.log(pageSize);
 	const UrlParametersArray =  [`q=${query}`,
                                  `page=${page}`, `apiKey=${apiKey}`,
 								 `pageSize=${pageSize}`]
